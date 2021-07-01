7e0600078b66f468b7bb46bf8c3328cc6c552eac
Hyxotis <chris.chich@hotmail.fr>
2021-07-01 18:30:03 +0200

    :bug: Removed unique constraint on name

--- src/repositories/restaurant.ts
+++ src/repositories/restaurant.ts
@@ -3,30 +3,27 @@ import mongoose from "../db/mongodb/index";
 const Schema = mongoose.Schema;
 
 const RestaurantSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        maxlength: 25,
-        unique: true,
-    },
-    description: {
-        type: String,
-        required: true,
-        default: null,
-        maxlength: 255
-    },
-    profilePicture: {
-        type: String,
-        required: true,
-        default: null,
-    },
-    profileId: {
-        type: String,
-        required: true,
-        default: null,
-    },
-
-
+  name: {
+    type: String,
+    required: true,
+    maxlength: 25,
+  },
+  description: {
+    type: String,
+    required: true,
+    default: null,
+    maxlength: 255,
+  },
+  profilePicture: {
+    type: String,
+    required: true,
+    default: null,
+  },
+  profileId: {
+    type: String,
+    required: true,
+    default: null,
+  },
 });
 
 const Restaurant = mongoose.model("Restaurant", RestaurantSchema);
